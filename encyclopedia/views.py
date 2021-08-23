ca7737d56e41e581bbf087c7b981e4cd4c04318d
Tewodros Asmamaw <tedabusiness@gmail.com>
2021-08-23 05:47:02 +0300

    Entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,6 @@
+from re import search
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
 
@@ -8,3 +9,18 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    mkd = Markdown()
+    entries = util.list_entries()
+    if title in entries:
+        page = util.get_entry(title)
+        mkd_page = mkd.convert(page)
+
+        content = {
+            'page': mkd_page,
+            'title': title
+        }
+        return render(request, "encyclopedia/entry.html", content)
+    else:
+        return render(request, "encyclopedia/error.html", {"display": "page not found!", "form": search()})
